80f8bc562133e58030e96bc59e26bec5ef0d4e15
Alma Navarro <deliang.ti20@utsjr.edu.mx>
2023-05-24 21:22:30 -0700

    add LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:contador/pages/home_page.dart';
+import 'package:contador/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,8 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: HomePage()
+      home: LoginPage()
     );
   }
 }
